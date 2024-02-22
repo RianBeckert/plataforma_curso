0aec67c40887be2013b634cf2fb6d46470aa9b4c
Your Name~Rian <you@example.comrian_beckert@hotmail.com>
2024-02-22 10:50:03 -0300

    Autenticação e tela de login

--- src/database/seeders/20240215223513-create-admin-user.js
+++ src/database/seeders/20240215223513-create-admin-user.js
@@ -1,24 +1,29 @@
-'use strict';
+const bcrypt = require("bcrypt");
+
+("use strict");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash("123456", 10);
+
+    await queryInterface.bulkInsert("users", [
+      {
+        first_name: "Admin",
+        last_name: "User",
+        phone: "555-5555",
+        birth: "1990-01-01",
+        email: "admin@email.com",
+        password: hashedPassword,
+        role: "admin",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {
+      where: { email: "admin@email.com" },
+    });
+  },
 };
